2bbc312da30459e85decd6b6fbfcab669de3a7c4
nodenwwsfww <nodenwwsfww@gmail.com>
2021-11-28 18:03:21 +0300

    test

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -8,25 +8,21 @@ import PropTypes from 'prop-types';
 
 import { Icon24FavoriteOutline, Icon24Search} from '@vkontakte/icons';
 
-const FooterMain = ({go}) => {
-    const [text, setText] = useState('search');
+const FooterMain = ({}) => {
+  const [text, setText] = useState('search');
 	return (
         <Tabbar style={{position: ''}} itemsLayout="vertical">
         <TabbarItem data-to="main" selected={text === 'search'} onClick={(e) => {
           setText('search');
-          go(e);
+          //go(e);
         }} text="Поиск"><Icon24Search/></TabbarItem>
         <TabbarItem data-to="favorites" selected={text === 'favorites'} 
         onClick={(e) => {
           setText('favorites');
-          go(e);
+          //go(e);
         }
         } text="Избранное"><Icon24FavoriteOutline/></TabbarItem>
         </Tabbar>
 	);
   };
-FooterMain.propTypes = {
-  go: PropTypes.func.isRequired,
-}; 
-  
 export default FooterMain;
