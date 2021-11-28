1ad29bbcb49985f6cc4c99986c048bcbfef9742f
nodenwwsfww <nodenwwsfww@gmail.com>
2021-11-28 14:32:35 +0300

    Получение карточек с сервера

--- src/panels/Cards.js
+++ src/panels/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { 
 	Group,
 	CardGrid,
@@ -7,28 +7,61 @@ import {
 import '@vkontakte/vkui/dist/vkui.css';
 
 const Cards = () => {
+    const [cards, setCards] = useState([
+        {
+            img: "https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+            name: "Университет ИТМО", 
+            description: "От 100 000 рублей в год", 
+            address: "м. Гостиный Двор, 22 мин. пешком Набережная реки Фонтанки, 4, подъезд 7" 
+        },
+        {
+            img: "https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+            name: "Университет ИТМО", 
+            description: "От 100 000 рублей в год", 
+            address: "м. Гостиный Двор, 22 мин. пешком Набережная реки Фонтанки, 4, подъезд 7" 
+        },
+        {
+            img: "https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+            name: "Университет ИТМО", 
+            description: "От 100 000 рублей в год", 
+            address: "м. Гостиный Двор, 22 мин. пешком Набережная реки Фонтанки, 4, подъезд 7" 
+        },
+    ]);
+
+    // + adding the use
+  useEffect(() => {
+    getData();
+
+    // we will use async/await to fetch this data
+    async function getData() {
+      const response = await fetch("https://www.anapioficeandfire.com/api/books");
+      const data = await response.json();
+
+      // store the data into our books variable
+      setCards(data) ;
+    }
+  }, []); // <- you may need to put the setBooks function in this array
+
 	return (
         <Group>
-            <CardGrid size="l">
+            {cards && (
+                <CardGrid size="l">
+                {cards.map((card, index) => (
                 <ContentCard
-                src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                header="Университет ИТМО"
-                text="От 100 000 рублей в год"
-                caption="м. Гостиный Двор, 22 мин. пешком
-                Набережная реки Фонтанки, 4, подъезд 7"
-                maxHeight={500}
-                >
+                    key={index}
+                    src={card.img}
+                    header={card.name}
+                    text={card.description}
+                    caption={card.address}
+                    maxHeight={500}
+                    >
                 </ContentCard>
-                <ContentCard
-                src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                header="Университет ИТМО"
-                text="От 100 000 рублей в год"
-                caption="м. Гостиный Двор, 22 мин. пешком
-                Набережная реки Фонтанки, 4, подъезд 7"
-                maxHeight={500}
-                />
-            </CardGrid>
+                ))}
+
+                </CardGrid>
+            )}
         </Group>
+
 	);
   };
 
