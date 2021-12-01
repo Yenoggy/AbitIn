cedae87e0c6d2d7f1cd70ad8123b271522da61b7
YENOGGY <57917541+Yenoggy@users.noreply.github.com>
2021-12-01 18:44:19 +0300

    add shit

--- src/components/HeaderSlider.js
+++ src/components/HeaderSlider.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { 
-	PanelHeader,
-	PanelHeaderButton,
+import {
+    PanelHeader,
+    PanelHeaderButton,
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import PropTypes from "prop-types";
 
-import { Icon20SlidersOutline} from '@vkontakte/icons';
+import {Icon20SlidersOutline} from '@vkontakte/icons';
 
 const HeaderSlider = ({setActiveModal}) => {
-	return (
+    return (
         <PanelHeader
-        left={<PanelHeaderButton data-modal="filters" onClick={setActiveModal}><Icon20SlidersOutline/></PanelHeaderButton>}
+            left={<PanelHeaderButton data-modal="filters"
+                                     onClick={setActiveModal}><Icon20SlidersOutline/></PanelHeaderButton>}
         >
-        AbitIn
+            AbitIn
         </PanelHeader>
-	);
-  };
+    );
+};
 
-  
+
 HeaderSlider.propTypes = {
-  setActiveModal: PropTypes.func.isRequired,
-};  
+    setActiveModal: PropTypes.func.isRequired,
+};
 export default HeaderSlider;
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { 
+import {
     ContentCard
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import PropTypes from 'prop-types';
 
 const SmallCard = ({id, img, name, description, address, go}) => {
-	return (
-      <div data-to="card">
-           <ContentCard onClick={go} data-to="card" 
-            src={img}
-            header={name}
-            text={description}
-            caption={address}
-            maxHeight={500}
+    return (
+        <div data-to="card">
+            <ContentCard onClick={go} data-to="card"
+                         src={img}
+                         header={name}
+                         text={description}
+                         caption={address}
+                         maxHeight={500}
             >
-        </ContentCard>
-      </div>
-	);
-  };
+            </ContentCard>
+        </div>
+
+);
+};
 
 SmallCard.propTypes = {
     id: PropTypes.number.isRequired,
@@ -27,5 +28,5 @@ SmallCard.propTypes = {
     description: PropTypes.string.isRequired,
     address: PropTypes.string.isRequired,
     go: PropTypes.func.isRequired,
-}; 
+};
 export default SmallCard;
